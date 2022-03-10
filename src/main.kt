acfb4d2b2c216ec4eda37f14e2be5e839a9220ee
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-03-10 08:59:24 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,38 @@
+fun main(){
+    var name = ("Codehive")
+    println(name[5])
+    println(name[6])
+    println(name[7])
+
+
+
+
+
+
+    var r= details("Sylviah",20)
+    println(r)
+
+    var u = lenght("sylviah")
+    println(u)
+
+
+
+fun module(num1:Int,num2:Int):Int{
+    var rem = (num1%num2)
+    return rem
+
+
+}
+}
+
+fun details(name:String,age:Int):String{
+    var history="Hi my name is $name,i am $age years old"
+    return history
+
+
+}
+fun lenght(name:String,):String{
+    return lenght
+
+
+}
